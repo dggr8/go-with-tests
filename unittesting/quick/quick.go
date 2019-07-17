9e1dd261c4f0136f2bc686e6775369911ce22182
Dhanya Ganesh <dgaaaaaaaaaa@gmail.com>
2019-07-16 22:38:46 -0600

    unittesting :  Go in Practice

---
+++ unittesting/quick/quick.go
@@ -0,0 +1,16 @@
+package quick
+
+import (
+	"log"
+	"strings"
+)
+
+func Pad(s string, max uint) string {
+	log.Printf("Testing Len: %d, Str: %s\n", max, s)
+	ln := uint(len(s))
+	if ln > max {
+		return s[:max]
+	}
+	s += strings.Repeat(" ", int(max-ln))
+	return s
+}
---
+++ unittesting/quick/quick_test.go
@@ -0,0 +1,17 @@
+package quick
+
+import (
+	"testing"
+	"testing/quick"
+)
+
+func TestPadGenerative(t *testing.T) {
+	fn := func(s string, max uint8) bool {
+		p := Pad(s, uint(max))
+		return len(p) == int(max)
+	}
+
+	if err := quick.Check(fn, &quick.Config{MaxCount: 200}); err != nil {
+		t.Error(err)
+	}
+}
